dac945086260d412136f4e03506507713f8893e3
mostafa-raihan <149680937+mostafa-raihan@users.noreply.github.com>
2023-11-08 12:07:02 +0200

    code changes

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-ef categorize_dict(x, y=0):
-    # x Requires string or numerical input
-    # y is a boolean that specifices whether to return category names along with the dict.
-    # default is no
-    cats = list(set(x))
-    n = len(cats)
-    m = len(x)
-    outs = {}
-    for i in cats:
-        outs[i] = [0]*m
-    for i in range(len(x)):
-        outs[x[i]][i] = 1
-    if y:
-        return outs,cat
+print "Hello"
+#nothing to do
+return 
+input
+if 
+else
